406d813f51b0de013f218f5c9957f3f210eeae3c
Dejan Joksimovic <joksimovic_d@yahoo.com>
2024-04-02 15:37:14 +0200

    pass param from outgoing route

--- src/components/shoppingList/ShoppingList.js
+++ src/components/shoppingList/ShoppingList.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useHistory } from 'react-router';
+import { useSelector } from 'react-redux'
 import { useLocation } from "react-router-dom";
 import MenuItem from '@material-ui/core/MenuItem';
 import { todoRoutePath } from '../todo/Todo';
+import { useDispatch } from 'react-redux';
+import { SET_TODO } from '../../Actions/ActionsTypes';
 
 export const ShoppingList = () => {
+    const dispatch = useDispatch()
     const {state: { number } = {}} = useLocation();
-    
+    const counter = useSelector(state => state.todo)
     const history = useHistory();
     const randomNumber = Math.random()*10
     const navigateTo = route => () => history.push(route);
     const replace = () => alert(randomNumber) || history.replace({ pathname: shoppingListRoutePath, state:{number: randomNumber}});
     const back = () => history.goBack();
+
+    useEffect(()=> {
+      if (counter) {
+        alert(counter)
+        dispatch({ type: SET_TODO, payload: { todo: null }})
+        history.replace({ pathname: shoppingListRoutePath, state:{number: counter}})
+      }
+    }, [])
 
     return (
         <>
--- src/components/todo/Todo.js
+++ src/components/todo/Todo.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { useHistory } from 'react-router';
+import { useDispatch } from 'react-redux'
 import MenuItem from '@material-ui/core/MenuItem';
 import { shoppingListRoutePath } from '../shoppingList/ShoppingList';
+import { SET_TODO } from '../../Actions/ActionsTypes';
 
 export const Todo = () => {
+    const dispatch = useDispatch()
     const history = useHistory();
     const randomNumber = Math.random()*10
     const navigateTo = route => () => history.push(route);
     const replace = () => alert(randomNumber) || history.replace({ pathname: shoppingListRoutePath, state:{number: randomNumber}});
-    const back = () => history.goBack();
+    const back = () => dispatch({ type: SET_TODO, payload: { todo: randomNumber }}) && history.goBack();
     return (
         <>
         <MenuItem onClick={back}>BACK</MenuItem>
